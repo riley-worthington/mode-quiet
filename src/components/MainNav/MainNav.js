6a908b44fc7eaa10194a2a6aac77af6ac57772de
Riley Worthington <worthirj@whitman.edu>
2018-09-12 15:09:47 -0700

    add links in top nav

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -8,13 +8,13 @@ const MainNav = () => {
         <img src="//static1.squarespace.com/static/5b2ee66d89c172da99e501aa/t/5b6e6dc08a922ddbba1412ed/1536713092955/?format=1500w" alt="modequiet" className="branding-logo" />
       </div>
       <div className="top-right-links">
-        <ul className="nav-ul">
-          <li>Home</li>
-          <li>Shop</li>
-          <li>Specifications</li>
-          <li>QuickStart</li>
-          <li>Fieldwork</li>
-        </ul>
+        <nav className="nav-bar">
+          <a href="">Home</a>
+          <a href="">Shop</a>
+          <a href="">Specifications</a>
+          <a href="">QuickStart</a>
+          <a href="">Fieldwork</a>
+        </nav>
         <div className='cart'>
           <img src={require("./cart_placeholder.png")} alt="cart" />
         </div>
